26e39e44ad6b9ab6557de23501898cb33705584a
2020chris <41304773+2020chris@users.noreply.github.com>
2018-08-05 20:35:44 -0400

    Add files via upload

---
+++ portal/firebase.js
@@ -0,0 +1,17 @@
+var app_firebase={};
+(function(){
+
+  // Initialize Firebase
+ var config = {
+    apiKey: "<API_KEY>",
+    authDomain: "<PROJECT_ID>.firebaseapp.com",
+    databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
+    projectId: "<PROJECT_ID>",
+    storageBucket: "<BUCKET>.appspot.com",
+    messagingSenderId: "<SENDER_ID>",
+  };
+  firebase.initializeApp(config);
+
+  app_firebase=firebase;
+
+})()
