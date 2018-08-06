c6ebc18705670daf1622d0e161a5a55b9b00ceac
2020chris <41304773+2020chris@users.noreply.github.com>
2018-08-06 19:54:46 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -18,35 +18,35 @@ function logOut(){
     firebase.auth().signOut();
 }
 function download()
-{
+{ 
     // Create a reference with an initial file path and name
 var storage = firebase.storage();
-var pathReference = storage.ref('images/stars.jpg');
+var pathReference = storage.ref('images/HR.jpg');
 
 // Create a reference from a Google Cloud Storage URI
 var gsReference = storage.refFromURL('gs://stroke-analysis.appspot.com/facescan/HR.jpg ')
 
 // Create a reference from an HTTPS URL
 // Note that in the URL, characters are URL escaped!
-var httpsReference = storage.refFromURL('https://firebasesto...-b8a9-3208608c5bd9');
-storageRef.child('images/stars.jpg').getDownloadURL().then(function(url) {
+//var httpsReference = storage.refFromURL('https://firebasestorage.googleapis.com/v0/b/stroke-analysis.appspot.com/o/facescan%2FHR.jpg?alt=media&token=f22c187e-0ce6-4661-b8a9-3208608c5bd9');
+//storageRef.child('images/stars.jpg').getDownloadURL().then(function(url) {
   // `url` is the download URL for 'images/stars.jpg'
 
   // This can be downloaded directly:
-  var xhr = new XMLHttpRequest();
-  xhr.responseType = 'blob';
-  xhr.onload = function(event) {
-    var blob = xhr.response;
-  };
-  xhr.open('GET', url);
-  xhr.send();
+ // var xhr = new XMLHttpRequest();
+  //xhr.responseType = 'blob';
+ // xhr.onload = function(event) {
+ //   var blob = xhr.response;
+  //};
+  //xhr.open('GET', url);
+  //xhr.send();
 
   // Or inserted into an <img> element:
-  var img = document.getElementById('myimg');
-  img.src = url;
-}).catch(function(error) {
+  //var img = document.getElementById('myimg');
+  //img.src = url;
+//}).catch(function(error) {
   // Handle any errors
-});
+//});
 
 }
 
