ed17b9614caf94a448844015becc58e1d11656da
2020chris <41304773+2020chris@users.noreply.github.com>
2018-08-06 20:26:38 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -19,7 +19,24 @@ function logOut(){
 }
 function download()
 { 
-  storageRef.child("facescan/HR.jpg").getDownloadUrl().getResult(); 
+  storageRef.child('images/stars.jpg').getDownloadURL().then(function(url) {
+    // `url` is the download URL for 'images/stars.jpg'
+  
+    // This can be downloaded directly:
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'blob';
+    xhr.onload = function(event) {
+      var blob = xhr.response;
+    };
+    xhr.open('GET', url);
+    xhr.send();
+  
+    // Or inserted into an <img> element:
+    var img = document.getElementById('myimg');
+    img.src = url;
+  }).catch(function(error) {
+    // Handle any errors
+  });
     // Create a reference with an initial file path and name
 //var storage = firebase.storage();
 //var pathReference = storage.ref('images/HR.jpg');
