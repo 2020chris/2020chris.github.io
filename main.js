bdc32402593dde30c7c96bc4306935768568a2fa
2020chris <41304773+2020chris@users.noreply.github.com>
2018-08-07 12:17:07 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -19,20 +19,20 @@ function logOut(){
 }
 function download()
 { 
-  storageRef.child('images/stars.jpg').getDownloadURL().then(function(url) {
+  storageRef.child('facescan/HR.jpg').getDownloadURL().then(function(url) {
     // `url` is the download URL for 'images/stars.jpg'
   
     // This can be downloaded directly:
-    var xhr = new XMLHttpRequest();
+   /* var xhr = new XMLHttpRequest();
     xhr.responseType = 'blob';
     xhr.onload = function(event) {
       var blob = xhr.response;
     };
     xhr.open('GET', url);
-    xhr.send();
+    xhr.send();*/
   
     // Or inserted into an <img> element:
-    var img = document.getElementById('myimg');
+    var img = document.getElementById('HR');
     img.src = url;
   }).catch(function(error) {
     // Handle any errors
